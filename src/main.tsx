653014e2ee12ceb3ba28f11b0fc21dd1f1ac45c9
ducothepaladin <lin303393@gmail.com>
2025-03-08 14:14:47 +0630

    feat: added loading component

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./style/app.css";
+import "./style/loading.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./routes/router.tsx";
 
