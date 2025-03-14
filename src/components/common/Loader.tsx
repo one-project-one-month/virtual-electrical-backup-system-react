e41110a39b01ec5919f82bc1f55840cb170d5bb4
ducothepaladin <lin303393@gmail.com>
2025-03-14 17:41:36 +0630

    update: loading ui

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -2,7 +2,7 @@ import "../../style/loading.css";
 
 export default function Loader() {
   return (
-    <div className="w-lvw h-lvh flex justify-center items-center">
+    <div className="w-lvw h-lvh flex justify-center fixed z-50 items-start">
         <div className="loader rounded-lg"></div>
     </div>
   )
